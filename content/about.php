644ad122aae2cfe97885d4823edbce0c5bfdefc5
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2022-06-06 21:56:54 +0700

    Update about.php

--- content/about.php
+++ content/about.php
@@ -1,6 +1,6 @@
 <?php
 $baseurl="
-        http://dexo.my.id/nu/
+       https://axcora.my.id/php-native/about.php
         " ;
 $cover="
         https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6284b53e-38f1-4d2e-81b4-f6a5c452f92f/d9o6yba-3071e4ea-982e-4654-b6f3-666027ab120c.png/v1/fill/w_1024,h_576,q_80,strp/php_wallpaper_by_milesandryprower_d9o6yba-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTc2IiwicGF0aCI6IlwvZlwvNjI4NGI1M2UtMzhmMS00ZDJlLTgxYjQtZjZhNWM0NTJmOTJmXC9kOW82eWJhLTMwNzFlNGVhLTk4MmUtNDY1NC1iNmYzLTY2NjAyN2FiMTIwYy5wbmciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.epU2KLIxu8aLPWf5yKw1oCTB0yEYKfyju6xmKdEL7xQ
