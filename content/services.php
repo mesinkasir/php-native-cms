75bce9f00028425d42293875db1d453b3be3a130
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2022-06-06 21:57:14 +0700

    Update services.php

--- content/services.php
+++ content/services.php
@@ -1,6 +1,6 @@
 <?php
 $baseurl="
-        http://dexo.my.id/nu/
+        https://axcora.my.id/php-native/services.php
         " ;
 $cover="
         https://media.istockphoto.com/photos/inscription-against-laptop-and-code-background-learn-php-programming-picture-id1297752994?b=1&k=20&m=1297752994&s=170667a&w=0&h=HGe7dfYaoX0cfBpFp16nptqI_d0QFsx3ij65ES9aJVQ=
